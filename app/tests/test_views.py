5de83c7d022b0e7c23a3fbf0cddc5fd6a3b0dbb7
Arthur Ngondo <ngondoarthur@gmail.com>
2018-05-30 11:18:09 +0300

    [Feature #157930463] Add failing tests

---
+++ app/tests/test_views.py
@@ -0,0 +1,60 @@
+""" Tests for views.py """
+from app import app
+import unittest
+import json
+
+
+class TestRouteCases(unittest.TestCase):
+    """ Test Cases """
+    def setUp(self):
+        """ Instantiate test client """
+        self.app = app.test_client()  # test client
+        # Sample user data
+        self.sample = {
+            'username': 'artorious',
+            'email': 'arthurngondo@email.com',
+            'password': 123454321,
+            'confirm_password': 123454321,
+            'first_name': 'Arthur',
+            'last_name': 'Ngondo'
+            }
+
+    def test_register_status(self):
+        """ Test endpoint sends HTTP POST request to the application."""
+        result = self.app.post(
+            '/api/v1/register', 
+            data=json.dumps(self.sample),
+            headers={'content-type': 'application/json'}
+        )
+        self.assertEqual(201, result.status_code, msg='Request POSTed')
+
+    def test_register_Success_operation_message(self):
+        """ Test endpoint returns message to user to indicate Pass/Fail """
+        result = self.app.post(
+            '/api/v1/register', 
+            data=json.dumps(self.sample),
+            headers={'content-type': 'application/json'}
+        )
+        self.assertIn('Account created', str(result.data))
+
+    def test_register_handles_bad_request(self):
+        """ Test endpoint returns message to user to indicate Bad request """
+        # 404 - Resource not found
+        result = self.app.post(
+            '/api/v1/register', 
+            data=json.dumps(self.sample),
+            headers={'content-type': 'application/json'}
+        )
+        self.assertEqual(404, result.status_code, msg='Resource not Found')
+
+    def test_register_Failed_operation_message(self):
+        """ Test endpoint returns message to user to indicate Pass/Fail """
+        result = self.app.post(
+            '/api/v1/registerss', 
+            data=json.dumps(self.sample),
+            headers={'content-type': 'application/json'}
+        )
+        self.assertIn('Account NOT created', str(result.data))
+
+if __name__ == '__main__':
+    unittest.main()
--- app/views.py
+++ app/views.py
@@ -1,2 +1,10 @@
 """ Routes for MaintenanceTracker App """
 from flask import request
+from app.models import Users
+from app import app
+
+
+@app.route('/api/v1/register', methods=['POST'])
+def register():
+    """Return msg to user - Success or Failed """
+    pass
